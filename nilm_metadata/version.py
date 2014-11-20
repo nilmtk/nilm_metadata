2a1e9f7f16827ba97a740b12cbbd364c98278136
Nipun <nipunreddevil@gmail.com>
2014-11-20 18:20:53 -0500

    Added versioning information

---
+++ nilm_metadata/version.py
@@ -0,0 +1,2 @@
+version = '0.2.0.dev'
+short_version = '0.2.0'
--- setup.py
+++ setup.py
@@ -1,5 +1,66 @@
 from setuptools import setup, find_packages
 
+from os.path import join
+import os
+import sys
+import warnings
+
+
+
+"""
+Following Segment of this file was taken from the pandas project(https://github.com/pydata/pandas) 
+"""
+# Version Check
+
+MAJOR = 0
+MINOR = 2
+MICRO = 0
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+QUALIFIER = ''
+
+FULLVERSION = VERSION
+if not ISRELEASED:
+    FULLVERSION += '.dev'
+    try:
+        import subprocess
+        try:
+            pipe = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
+                                    stdout=subprocess.PIPE).stdout
+        except OSError:
+            # msysgit compatibility
+            pipe = subprocess.Popen(
+                ["git.cmd", "rev-parse", "--short", "HEAD"],
+                stdout=subprocess.PIPE).stdout
+        rev = pipe.read().strip()
+        # makes distutils blow up on Python 2.7
+        if sys.version_info[0] >= 3:
+            rev = rev.decode('ascii')
+
+        FULLVERSION += "-%s" % rev
+    except:
+        warnings.warn("WARNING: Couldn't get git revision")
+else:
+    FULLVERSION += QUALIFIER
+
+
+def write_version_py(filename=None):
+    cnt = """\
+version = '%s'
+short_version = '%s'
+"""
+    if not filename:
+        filename = os.path.join(
+            os.path.dirname(__file__), 'nilm_metadata', 'version.py')
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % (FULLVERSION, VERSION))
+    finally:
+        a.close()
+write_version_py()
+# End of Version Check
+
 setup(
     name='nilm_metadata',
     version='0.2.0',
