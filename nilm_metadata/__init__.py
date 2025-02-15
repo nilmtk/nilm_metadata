f71fa67a36e0bc3783a68892882d378bd2bbe96e
Mihail Vorobiov <mihail.vorobiov@gmail.com>
2025-02-15 17:41:52 +0200

    switch from conda to setuptools/wheel

--- nilm_metadata/__init__.py
+++ nilm_metadata/__init__.py
@@ -1,6 +1,5 @@
 from .object_concatenation import get_appliance_types, recursively_update_dict
 from .convert_yaml_to_hdf5 import convert_yaml_to_hdf5, save_yaml_to_datastore
-from nilm_metadata.version import version as __version__
 
 import os
 
